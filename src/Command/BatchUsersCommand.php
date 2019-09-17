036b68247b9bba10f6cb4dee20addf45b343842b
Gleimer Mora <gleimer.mora@ladbrokes.com.au>
2019-09-17 21:22:11 +1000

    Add command options, add csv file parser and email validator

---
+++ src/Command/BatchUsersCommand.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Command;
+
+use App\Helper\File;
+use Exception;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class BatchUsersCommand extends Command
+{
+    /** @var string $defaultName */
+    protected static $defaultName = 'batch-users';
+
+    /** @var string $file */
+    private $file = '';
+
+    /** @var bool $createTable */
+    private $createTable = false;
+
+    /** @var bool $dryRun */
+    private $dryRun = false;
+
+    /** @var string $dbUser */
+    private $dbUser = '';
+
+    /** @var string $dbPassword */
+    private $dbPassword = '';
+
+    /** @var string $dbHost */
+    private $dbHost = '';
+
+    /** @var string $description */
+    private  $description = <<<DESC
+PHP script, that is executed from the command line, which accepts a CSV file as input
+(see command line directives below) and processes the CSV file. The parsed file data is to be
+inserted into a MySQL database.
+DESC;
+
+    protected function configure()
+    {
+        $this
+            ->setDescription($this->description)
+            ->addOption('file', 'f', InputOption::VALUE_REQUIRED, 'name of the CSV to be parsed')
+            ->addOption('create-table', 'c', InputOption::VALUE_OPTIONAL, 'MySQL users table to be built')
+            ->addOption('dry-run', 'd', InputOption::VALUE_OPTIONAL, 'used with the --file directive in case we want to run the
+script but not insert into the DB', false)
+            ->addOption('db-user', 'u', InputOption::VALUE_OPTIONAL, 'MySQL username')
+            ->addOption('db-password', 'p', InputOption::VALUE_OPTIONAL, 'MySQL password')
+            ->addOption('db-host', null, InputOption::VALUE_OPTIONAL, 'MySQL host')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Batch Users');
+
+        $this->processOptions($input->getOptions());
+
+        try {
+            list($errors, $records) = (new File)->parseCsv($this->file);
+        } catch (Exception $e) {
+            $output->writeln("<error>{$e->getMessage()}</error>");
+            exit(128);
+        };
+        if (count($errors)) {
+            $io->listing($errors);
+        }
+        if ($this->dryRun) {
+            $io->title('Dry run, DB operations are not executed');
+            exit(0);
+        }
+    }
+
+    /**
+     * @param array $options
+     */
+    private function processOptions(array $options = [])
+    {
+        $this->file = $options['file'] ?? '';
+        $this->dryRun = $options['dry-run'] ?? true;
+    }
+}
---
+++ src/Helper/File.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Helper;
+
+use Iterator;
+use League\Csv\Reader;
+use Symfony\Component\Console\Exception\InvalidOptionException;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validation;
+
+class File
+{
+    /**
+     * @param string $path
+     * @return array
+     */
+    public function parseCsv(string $path = ''): array
+    {
+        if ($path === '') {
+            throw new InvalidOptionException('The --file option requires a value.');
+        }
+
+        // read file from path
+        $csvFile = Reader::createFromPath($path, 'r');
+        if ($csvFile->count() === 0) {
+            throw new InvalidOptionException('Could not read records, No such file.');
+        }
+        $csvFile->setHeaderOffset(0);
+        return $this->validateRecords($csvFile->getRecords());
+    }
+
+    /**
+     * @param Iterator $records
+     * @return array
+     */
+    private function validateRecords(Iterator $records): array
+    {
+        $emailConstraint = new Assert\Email([
+            'message' => 'The email {{ value }} is not a valid email.',
+            'checkMX' => true,
+        ]);
+        $blankConstraint = new Assert\NotBlank(['message' => '{{ value }} should not be blank.']);
+
+        $validator = Validation::createValidator();
+        $validatedRecords = [];
+        $errors = [];
+        foreach ($records as $record) {
+            $name = trim($record['name']) ?? '';
+            $surname = trim($record['surname']) ?? '';
+            $email = trim($record['email']) ?? '';
+            $violations = $validator->validate($email, $emailConstraint);
+            foreach ($violations as $violation) {
+                $errors[] = $violation->getMessage();
+            }
+            $violations2 = $validator->validate($name, $blankConstraint);
+            foreach ($violations2 as $violation) {
+                $errors[] = 'Name ' . $violation->getMessage();
+            }
+            if (count($errors)) {
+                continue;
+            }
+            $validatedRecords[] = [ucfirst(strtolower($name)), ucfirst(strtolower($surname)), $email];
+        }
+        return [$errors, $validatedRecords];
+    }
+}
